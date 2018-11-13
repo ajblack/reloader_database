fb4e1553c78a2781a9430dc859c45c29fa0a60ce
Austin Blackman <austin.blackman@gmail.com>
2018-11-12 23:00:41 -0500

    fixed a couple tests

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -16,12 +16,6 @@ describe('AppComponent', () => {
   it(`should have as title 'angular5-example'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular5-example');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular5-example!');
+    expect(app.title).toEqual('Reloader DB');
   }));
 });
